4ccecbd6dc6371d2236fbc3e7c8638071d874062
Ganzeroar <ganzeroar@gmail.com>
2023-04-24 20:39:33 +0600

    remove status field

--- backend/crm/serializers/orders.py
+++ backend/crm/serializers/orders.py
@@ -1,12 +1,18 @@
-from crm.models import Orders
+from crm.models import Orders, OrderStatus
 from rest_framework import serializers
 from .clients import ClientsSerializer
 
 class SpecificOrdersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Orders
-        fields = '__all__'
+        fields = ['quantity', 'product', 'client']
 
+    def create(self, validated_data):
+        status = OrderStatus.objects.get(name='В работе')
+        validated_data['status'] =  status
+        obj = Orders.objects.create(**validated_data)
+        obj.save()
+        return obj
 
 
 class OrdersSerializer(serializers.ModelSerializer):
