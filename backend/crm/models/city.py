541ff11930e21325d7fde457b476004a4101be1f
Dmitriy <ganzeroar@gmail.com>
2023-05-11 17:23:43 +0600

    add fixtures

--- backend/crm/models/city.py
+++ backend/crm/models/city.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class City(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name='Город')
 
     class Meta:
         verbose_name = 'Город'
